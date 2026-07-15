e9771ceebeb24e00b58012e3a1fd5433bdf97212
davideinfire <davidefr@proton.me>
2026-07-15 18:53:29 +0300

    Create journal.py

---
+++ journal.py
@@ -0,0 +1,13 @@
+class Journal:
+
+    def __init__(self):
+
+        self.entries = []
+
+    def add(self, entry):
+
+        self.entries.append(entry)
+
+    def all(self):
+
+        return self.entries
