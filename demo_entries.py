6eb57301c96b90a61990709bfb003ca0569aead5
davideinfire <davidefr@proton.me>
2026-07-15 18:53:55 +0300

    Create demo_entries.py

---
+++ demo_entries.py
@@ -0,0 +1,23 @@
+from entry import Entry
+
+ENTRIES = [
+
+    Entry(
+        "2026-07-13",
+        "Happy",
+        "Finished reading a new book."
+    ),
+
+    Entry(
+        "2026-07-14",
+        "Calm",
+        "Cleaned the workspace and organized files."
+    ),
+
+    Entry(
+        "2026-07-15",
+        "Happy",
+        "Built another GitHub project."
+    )
+
+]
